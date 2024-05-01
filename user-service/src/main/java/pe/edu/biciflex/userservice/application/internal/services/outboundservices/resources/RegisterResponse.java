ee9e117771c04625c65729e7bb4c12883de6cda8
Adrian Melgar <DaikiMT@outlook.es>
2024-05-01 16:25:27 -0500

    Added user-service

---
+++ user-service/src/main/java/pe/edu/biciflex/userservice/application/internal/services/outboundservices/resources/RegisterResponse.java
@@ -0,0 +1,15 @@
+package pe.edu.biciflex.userservice.application.internal.services.outboundservices.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RegisterResponse {
+    private String email;
+    private String password;
+}
